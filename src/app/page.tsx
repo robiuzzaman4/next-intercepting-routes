0993d9850f1e99c0e6bd9571fce734030db9389f
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-07-04 18:48:56 +0600

    feat: update github build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,10 @@ const Home = () => {
             Intercepting Routes.
           </Link>{" "}
         </span>
-        <Link href={`/products/`} className="w-full">
+        <Link
+          href={`https://github.com/robiuzzaman4/next-intercepting-routes`}
+          className="w-full"
+        >
           <button className="inline-flex items-center justify-center gap-2 px-4 py-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-zinc-950 bg-zinc-950 shadow-sm hover:bg-zinc-950/90 text-white">
             <span>Github</span>
             <GithubIcon className="text-white" size={16} />
